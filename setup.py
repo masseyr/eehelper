3d94d68792e2a3ee683f129f899da43f25e81c94
Richard Massey <rm885@nau.edu>
2020-04-05 18:06:38 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,21 +5,29 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="eehelper",
-    version="0.1.1",
+    version="0.1.2",
     author="Richard Massey",
     author_email="rm885@nau.edu",
     description="Helper library for Google Earth Engine python API scripts",
+    license='MIT License',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/masseyr/eehelper",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Topic :: Scientific/Engineering :: GIS',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
     install_requires=[
         'earthengine-api>=0.1.175',
-    ]
+    ],
+    keywords='geospatial earthengine spatial google earth science satellite landsat modis',
 )
